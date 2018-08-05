4d6d0beb7444ebe4c8664c8f35f1070d92eb50f3
ipekkarakaya <obiii89@googlemail.com>
2018-08-05 17:01:18 +0200

    1

--- rest/Led.py
+++ rest/Led.py
@@ -14,4 +14,15 @@ class Led(object):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.led,GPIO.OUT)
         GPIO.output(self.led,False)
-        GPIO.cleanup(self.led)
+
+    def activateButton(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(led, GPIO.OUT)
+        GPIO.add_event_detect(23, GPIO.RISING, callback=changeLed, bouncetime=300)
+
+    def changeLed(self, channel):
+        if (GPIO.input(led) == True):
+            GPIO.output(led, False)
+        else:
+            GPIO.output(led, True)
