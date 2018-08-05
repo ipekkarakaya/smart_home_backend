6f3feb7b5363b5e8bad8d6c41aa23db01bf839c8
ipekkarakaya <obiii89@googlemail.com>
2018-08-05 15:45:47 +0200

    1

--- electronics/button2.py
+++ electronics/button2.py
@@ -1,8 +1,14 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
 
-test = 1
+GPIO.cleanup()
+
+GPIO.setmode(GPIO.BCM)
+def changeLed(channel):
+    if ledState == True:
+        GPIO.output(23, False)
+    else:
+        GPIO.output(23, True)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
 GPIO.setup(14, GPIO.OUT)  #LED to GPIO14
@@ -10,9 +16,3 @@ GPIO.setup(14, GPIO.OUT)  #LED to GPIO14
 GPIO.add_event_detect(23, GPIO.RISING, callback=changeLed)
 
 ledState = GPIO.output(14)
-
-def changeLed(channel):
-    if ledState == True:
-        GPIO.output(23, False)
-    else:
-        GPIO.output(23, True)
