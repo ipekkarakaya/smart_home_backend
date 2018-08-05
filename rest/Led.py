a3fa855922e69f08c3494685bb9ce5a18b836ec7
ipekkarakaya <obiii89@googlemail.com>
2018-08-05 16:55:55 +0200

    1

--- rest/Led.py
+++ rest/Led.py
@@ -2,15 +2,16 @@ import RPi.GPIO as GPIO
 
 class Led(object):
     def __init__(self):
-        pass
+        self.button = 23
+        self.led = 14
 
     def on(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(26,GPIO.OUT)
-        GPIO.output(26,True)
+        GPIO.setup(led,GPIO.OUT)
+        GPIO.output(led,True)
 
     def off(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(26,GPIO.OUT)
-        GPIO.output(26,False)
-        GPIO.cleanup(26)
+        GPIO.setup(led,GPIO.OUT)
+        GPIO.output(led,False)
+        GPIO.cleanup(led)
