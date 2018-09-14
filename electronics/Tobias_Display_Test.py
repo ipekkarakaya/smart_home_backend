0a036d54d0d88b0793a92f0ea139b02e087d8cfd
Tobias <obiii89@googlemail.com>
2018-09-14 19:49:28 +0200

    1

--- electronics/Tobias_Display_Test.py
+++ electronics/Tobias_Display_Test.py
@@ -45,7 +45,7 @@ font36 = ImageFont.truetype('Roboto-Medium.ttf', 36)
 
 def showText(channel):
     draw.rectangle((0,0,width,height), outline=0, fill=0)
-    draw.text((10, 40),    '24.5 *C',  font=font36, fill=255)
+    draw.text((10, 10),    '24.5 *C',  font=font36, fill=255)
     disp.image(image)
     disp.display()
 
