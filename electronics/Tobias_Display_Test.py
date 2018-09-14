4a2ec33ab00e782a0e96f10ed488cd79929b2792
Tobias <obiii89@googlemail.com>
2018-09-14 19:48:26 +0200

    1

--- electronics/Tobias_Display_Test.py
+++ electronics/Tobias_Display_Test.py
@@ -37,7 +37,7 @@ font = ImageFont.load_default()
 font18 = ImageFont.truetype('Roboto-Medium.ttf', 18)
 font20 = ImageFont.truetype('Roboto-Medium.ttf', 20)
 font24 = ImageFont.truetype('Roboto-Medium.ttf', 24)
-font48 = ImageFont.truetype('Roboto-Medium.ttf', 48)
+font36 = ImageFont.truetype('Roboto-Medium.ttf', 36)
 #draw.text((x, top),    'Joe Mama!',  font=font18, fill=255)
 # draw.text((x, top+18), 'DHTxx', font=font24, fill=255)
 
@@ -45,7 +45,7 @@ font48 = ImageFont.truetype('Roboto-Medium.ttf', 48)
 
 def showText(channel):
     draw.rectangle((0,0,width,height), outline=0, fill=0)
-    draw.text((0, 0),    '24.5 *C',  font=font48, fill=255)
+    draw.text((0, 0),    '24.5 *C',  font=font36, fill=255)
     disp.image(image)
     disp.display()
 
